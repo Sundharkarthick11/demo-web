fb133e0a0e5cf517456e3b16c63bd2be570f606d
Sundharkarthick11 <rajendransubburam@gmail.com>
2023-12-10 20:10:35 +0530

    modified

---
+++ Bookcake/script.js
@@ -0,0 +1,39 @@
+//selecting the popup button, popup overlay, popup window
+
+var popupbutton=document.getElementById("add-popup-button")
+var _overlay=document.querySelector(".popup-overlay")
+var _window=document.querySelector(".popup-window")
+
+popupbutton.addEventListener("click", function(){
+    _overlay.style.display="block";
+    _window.style.display="block";
+})
+//selecting cancel button
+var _cancelbutton=document.querySelector(".cancel-popup")
+
+_cancelbutton.addEventListener("click", function(){
+    _overlay.style.display="none";
+    _window.style.display="none";
+})
+//selecting container, book-name-input, book-author-input,book-description, add button
+var _container=document.querySelector(".container")
+var bookname=document.querySelector(".book-name-input")
+var bookauthor=document.querySelector(".book-author-input")
+var bookdescrip=document.querySelector(".book-description")
+var _addbutton=document.querySelector(".add-book")
+
+_addbutton.addEventListener("click", function(event){
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${bookname.value}</h2>
+    <p>${bookauthor.value}</p>
+    <button>${bookdescrip.value}</button>`
+    _container.append(div)
+    _overlay.style.display="none";
+    _window.style.display="none";
+})
+
+function deletefunc(event){
+    event.target.parentElement.remove()
+}
+
