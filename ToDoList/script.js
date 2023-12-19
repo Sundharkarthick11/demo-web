76d05541293dffc18d32c468a57e8fa996c35bb1
Sundharkarthick11 <rajendransubburam@gmail.com>
2023-12-19 10:28:39 +0530

    modified

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -4,11 +4,11 @@ function update(event){
     var user_inpt=document.getElementById("input-text")
     //selecting div
     var divu=document.querySelector(".res")
-    var item=document.createElement("p")
-    item.setAttribute("class","para")
-    item.textContent=user_inpt.value
+    var item=document.createElement("div")
+    item.innerHTML=`<p class="para">${user_inpt.value}</p><button class="deletebutton" onclick="deletefunc(event)">x</button>`
     divu.append(item)
-    user_inpt.value=''
-    
+    user_inpt.value=''  
 }
-
+function deletefunc(event){
+    event.target.parentElement.remove()
+}
